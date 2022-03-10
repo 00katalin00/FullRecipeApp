02cf149dfce090644387cc04faf5e943abaca21e
C. Gligor <00katalin00@gmail.com>
2022-03-10 21:01:34 +0100

    new pack

--- frontend/src/components/Popular.jsx
+++ frontend/src/components/Popular.jsx
@@ -1,21 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
+
   useEffect(() => {
     getPopular();
   }, []);
 
   const getPopular = async () => {
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=15`
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
 
     const data = await api.json();
-    //console.log(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=15`);
-    console.log(data);
+
+    setPopular(data.recipes);
   };
 
-  return <div>Popular</div>;
+  return (
+    <div>
+      {popular.map((r) => {
+        return(
+          <div key={r.id} >
+            <p>{r.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Popular;
