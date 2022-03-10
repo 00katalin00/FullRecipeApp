d5b19168d8e46bc487f48115d3fa16e102ffb43b
C. Gligor <00katalin00@gmail.com>
2022-03-10 22:04:58 +0100

    localstorage

--- frontend/src/components/Popular.jsx
+++ frontend/src/components/Popular.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -8,26 +12,90 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
+    const check = localStorage.getItem("popular");
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`
+      );
+      const data = await api.json();
 
-    const data = await api.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
 
-    setPopular(data.recipes);
+      setPopular(data.recipes);
+    }
   };
 
   return (
-    <div>
-      {popular.map((r) => {
-        return(
-          <div key={r.id} >
-            <p>{r.title}</p>
-          </div>
-        );
-      })}
-    </div>
+    <Wrapper>
+      <h3>Fotos destacadas</h3>
+      <Splide
+        options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: "free",
+          grap: "5 rem",
+        }}
+      >
+        {popular.map((r) => {
+          return (
+            <SplideSlide key={r.id}>
+              <Card>
+                <p>{r.title}</p>
+                <img src={r.image} alt={r.title} />
+                <Gradient />
+              </Card>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+const Card = styled.div`
+  min-height: 24rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 1rem;
+    position: absolute;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1 rem;
+    height: 20%;
+    disple: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 
 export default Popular;
